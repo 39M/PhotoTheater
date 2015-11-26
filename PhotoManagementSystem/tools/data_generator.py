2400114a4dddfc6daadda376789e09e3b8abb7b1
Jiaqi Wang <xwszc7@gmail.com>
2015-11-26 17:35:39 +0800

    generate shot_time and position

--- PhotoManagementSystem/tools/data_generator.py
+++ PhotoManagementSystem/tools/data_generator.py
@@ -2,6 +2,7 @@
 import os
 import random
 from django.utils import timezone
+from datetime import datetime
 from django.core.files import File
 from PhotoManager.models import *
 
@@ -30,12 +31,18 @@ files = os.listdir('tools/')
 files.remove('data_generator.py')
 for img in files:
     img.decode('utf-8')
+    year = str(random.randint(2010, 2015))
+    month = str(random.randint(1, 12)).zfill(2)
+    day = str(random.randint(1, 28)).zfill(2)
+    date_str = year + '-' + month + '-' + day
     p = Photo(
         album=albums[random.randint(0, len(albums) - 1)],
         name=img.split('.')[0],
-        # shot_date=timezone.now(),
+        shot_date=datetime.strptime(date_str, '%Y-%m-%d'),
         # upload_date=timezone.now(),
         # update_date=timezone.now(),
+        latitude=random.uniform(45, 46),
+        longitude=random.uniform(126, 127),
     )
     tmp = File(open('tools/' + img, 'rb'))
     p.source = tmp
