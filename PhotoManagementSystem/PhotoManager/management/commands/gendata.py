f06e84070ab71167a660c4fab03c747288c153d1
Jiaqi Wang <xwszc7@gmail.com>
2015-12-23 17:06:47 +0800

    fix conflict

--- PhotoManagementSystem/PhotoManager/management/commands/gendata.py
+++ PhotoManagementSystem/PhotoManager/management/commands/gendata.py
@@ -66,10 +66,7 @@ class Command(BaseCommand):
                 # Create thumb
                 thumb.thumbnail((480, 200), Image.ANTIALIAS)
                 thumb.save('tools/thumb/' + img + '.thumbnail', 'JPEG')
-<<<<<<< HEAD
-                # thumb.close()
-=======
->>>>>>> origin/master
+
                 # Save thumb
                 thumb = File(open('tools/thumb/' + img + '.thumbnail', 'rb'))
                 thumb.name = tmp.name
